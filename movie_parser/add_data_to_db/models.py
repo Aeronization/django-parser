515facc83626dffffe481838934489a0f2ae0f33
Ryan <rfriel@rezjam.com>
2021-01-12 16:00:45 -0500

    Created the two available models. Movie and Genre.

--- movie_parser/add_data_to_db/models.py
+++ movie_parser/add_data_to_db/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# (R. Friel - January 12, 2021) - I am creating the models here based on the Schema present in the README.txt available from the zip file. - 
+# Because a movie can have multiple generes I created a second model for Genre, and will reference it to the Movie model
+# via ManyToMany field. As a Movie can have many genres, and a Genre can be related to many different movies.
+
+
+class Genre(models.Model):
+    genre_name = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Movie(models.Model):
+    movie_id = models.CharField(max_length=128)
+    movie_title = models.CharField(max_length=128)
+    movie_year = models.CharField(max_length=128)
+    imbd_rating = models.FloatField()
+    number_of_imdb_votes = models.IntegerField()
+    genre = models.ManyToManyField(Genre, blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
